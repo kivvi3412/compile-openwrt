c456c3b534e5cf5a400b05ec2d306a566c708e35
ztfot <64115433+ztfot@users.noreply.github.com>
2020-09-23 22:58:19 +0800

    Update openwrt.sh

--- openwrt.sh
+++ openwrt.sh
@@ -1,7 +1,7 @@
 sudo apt update -y && sudo apt upgrade -y 
 sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3.5 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf
 sudo apt autoremove -y && sudo apt autoclean -y
-cd ~/Downloads
+cd ~/
 git clone https://github.com/coolsnowwolf/lede
 cd lede
 echo 'src-git packages https://github.com/coolsnowwolf/packages
@@ -17,5 +17,4 @@ src-git small https://github.com/V2RaySSR/small' > feeds.conf.default
 make menuconfig
 make -j8 download V=s && make -j8 download V=s && make -j8 download V=s
 make -j1 V=s
-mv bin/targets/* ~/Downloads/
-echo "\033[43;31mSave to ~/Downloads/ \033[0m"
+
