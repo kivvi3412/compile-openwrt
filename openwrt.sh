b99a2d75d2f7379897e33db0537242f9fe250264
kivvi3412 <64115433+kivvi3412@users.noreply.github.com>
2022-11-08 21:33:04 +0800

    Update openwrt.sh

--- openwrt.sh
+++ openwrt.sh
@@ -1,9 +1,11 @@
-sudo apt update -y && sudo apt upgrade -y 
-sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3.5 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf
-sudo apt autoremove -y && sudo apt autoclean -y
-mkdir ~/OpenWrt
-mkdir ~/OpenWrt/result/
-cd ~/OpenWrt
+sudo apt update -y
+sudo apt full-upgrade -y
+sudo apt install -y ack antlr3 aria2 asciidoc autoconf automake autopoint binutils bison build-essential \
+bzip2 ccache cmake cpio curl device-tree-compiler fastjar flex gawk gettext gcc-multilib g++-multilib \
+git gperf haveged help2man intltool libc6-dev-i386 libelf-dev libglib2.0-dev libgmp3-dev libltdl-dev \
+libmpc-dev libmpfr-dev libncurses5-dev libncursesw5-dev libreadline-dev libssl-dev libtool lrzsz \
+mkisofs msmtp nano ninja-build p7zip p7zip-full patch pkgconf python2.7 python3 python3-pip libpython3-dev qemu-utils \
+rsync scons squashfs-tools subversion swig texinfo uglifyjs upx-ucl unzip vim wget xmlto xxd zlib1g-dev
 git clone https://github.com/coolsnowwolf/lede
 cd lede
 echo "src-git packages https://github.com/coolsnowwolf/packages
@@ -18,5 +20,3 @@ src-git small https://github.com/kenzok8/small" > feeds.conf.default
 ./scripts/feeds install -a
 make menuconfig
 make -j8 download V=s && make -j8 download V=s && make -j8 download V=s && make -j1 V=s
-mv bin/targets/* ~/OpenWrt/result/
-echo "\033[43;31mSave to ~/OpenWrt/ \033[0m"
